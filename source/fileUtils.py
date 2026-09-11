11ce09ccdc8be3fdb37f65c56421a954b20fcc44
Leonard de Ruijter <alderuijter@gmail.com>
2026-09-11 09:52:42 +0200

    Smoke test: deliberate ruff-format violation (throwaway)

--- source/fileUtils.py
+++ source/fileUtils.py
@@ -127,3 +127,11 @@ def isDirEmpty(dir: str) -> bool:
 	"""
 	with os.scandir(dir) as scanner:
 		return not any(scanner)
+
+
+def _smokeTest( a,b ):
+	return a+b
+
+
+def _smokeTest( a,b ):
+	return a+b
